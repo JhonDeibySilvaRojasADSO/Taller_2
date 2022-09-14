23b6c754d752ab351cd129c7050409446c1c66dc
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-14 10:09:31 -0500

    ejercicio 5

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 addEventListener("DOMContentLoaded", ()=>{
-    //datos de entrada
-    //cuanto dinero tenia 
-    let lla=15000
-    let bom=1200000
-    let per=90000
-    let sobra=91000
-    //procesos u operaciones
-    let jue=(lla*5)
-    let caj=(per*3)
-    let tot=(jue+bom+per+sobra)
-    //salidad o entrega de resultados 
-    console.log("cuanto dinero tenia en total:",tot)
+    //datos de entrada 
+    //cantidad de gramos 
+    let har=100
+    //procesoso u operaciones
+    let gram=(2*100)
+    //salida o entrega de resultados 
+    console.log("gramos de harina necesarios:",gram)
 })
