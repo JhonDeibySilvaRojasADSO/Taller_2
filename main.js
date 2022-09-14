a2683efbb827f92f985fd47dbfb4d43fc6753380
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-14 10:00:01 -0500

    ejercicio2

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 addEventListener("DOMContentLoaded", ()=>{
     //datos de entrada
-    //calcule la distancia
-    let rc=5
-    let x=7
-    let y=3
+    //operaciones basicas
+    let pri1=Number(prompt("dijite el valor del producto# 1 :"))
+    let pri2=Number(prompt("dijite el valor del producto# 2:"))
+    let pri3=Number(prompt("dijite el valor del producto# 3 :"))
+    let pri4=Number(prompt("dijite el valor del producto# 4 :"))
+    let pri5=Number(prompt("dijite el valor del producto# 5 :"))
     //procesos u operaciones
-    let d=rc*((x*2-x*1)**2+(y*2-y*1)**2)
-    //salida o entrga de resultados
-    console.log("distancia calculada:",d)
+    let des1=pri1-(pri1*0.05)
+    let des2=pri2-(pri2*0.05)
+    let des4=pri4-(pri4*0.02)
+    let des5=pri5-(pri5*0.02)
+    let des6=des1+des2+pri3+des4+des5
+    //salida o entrada de resultados
+    console.log("valor de los 2 primeros productos son:",des1,"y",des2)
+    console.log("valor del tercer producto es:",pri3)
+    console.log("valor de los dos ultimos productos son:",des4,"y",des5)
+    console.log("valor de toda la compra es de:",des6)
 })
