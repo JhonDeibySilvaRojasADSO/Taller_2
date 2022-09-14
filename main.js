706b80afb0a6e1a8ac8932a9de37f4f8961d88d7
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-14 09:53:28 -0500

    ejercicio 1

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 1. Escribir un programa que sume, reste, multiplique y divida dos números
-    
-    let num1 = Number(prompt("Ingrese el numero 1", 2));
-    let num2 = Number(prompt("Ingrese el numero 2", 2));
-
-    console.log(`La suma es ${num1} + ${num2} = ${num1 + num2}`);
-    console.log(`La resta es ${num1} - ${num2} = ${num1 - num2}`);
-    console.log(`La multiplicacion es ${num1} * ${num2} = ${num1 * num2}`);
-    console.log(`La division es ${num1} / ${num2} = ${num1 / num2}`);
-
-
-
-
-
-
-
+    //datos de entrada
+    //calcule la distancia
+    let rc=5
+    let x=7
+    let y=3
+    //procesos u operaciones
+    let d=rc*((x*2-x*1)**2+(y*2-y*1)**2)
+    //salida o entrga de resultados
+    console.log("distancia calculada:",d)
 })
