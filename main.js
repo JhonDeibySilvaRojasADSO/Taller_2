b6dfcfb61d87159924cc6ad6b92e4ab21d4de7fa
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-14 10:16:51 -0500

    ejercicio 7

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 addEventListener("DOMContentLoaded", ()=>{
     //datos de entrada
-    //cuantos kg transporta
-    let caj=25
-    let pes1=748
-    //precesos u operaciones 
-    let trans=(pes1*caj)
-    //salida o entrega de resultados 
-    console.log("cuantos kg transporta en total:",trans)
+    //cantidad de tapas
+    let tap=25000
+    //procesos u operaciones 
+    let m=(2/3)*tap
+    let j=(1/4)*tap
+    let sum=(m+j)
+    let l=(tap-sum)
+    //salida o entrega de resultados
+    console.log("cantidad de tapas corrspondientes a lorena:",l)
 })
