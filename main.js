da53318bbf561c537b038c2b5a93721622744f10
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-09-14 10:04:49 -0500

    ejercicio 4

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 addEventListener("DOMContentLoaded", ()=>{
-//datos de entrada
-//tiempo estimo para llenar un dispositivo
-let pi=3.1416
-let rad=4
-let h=15
-let t=40
-//procesos u operaciones
-let v=pi*(rad**2)*h
-let ti=(t*60)
-let q=(v/ti)
-// salida o entrega de resultados
-console.log("volomen:",v)
-console.log("caudal:",q)
+    //datos de entrada
+    //cuanto dinero tenia 
+    let lla=15000
+    let bom=1200000
+    let per=90000
+    let sobra=91000
+    //procesos u operaciones
+    let jue=(lla*5)
+    let caj=(per*3)
+    let tot=(jue+bom+per+sobra)
+    //salidad o entrega de resultados 
+    console.log("cuanto dinero tenia en total:",tot)
 })
